0fa0afdb8d1e80c7fd2a111f37b396bb82d6bb37
loc <hoanglocst900@gmail.com>
2022-05-27 17:36:02 +0700

    ADD - get all blog categories

---
+++ src/blog-category/blog-categories.controller.ts
@@ -0,0 +1,17 @@
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { BlogCategoryService } from './blog-category.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+@ApiTags('blog-categories')
+@ApiBearerAuth()
+@Controller('blog-categories')
+export class BlogCategoriesController {
+    constructor(private readonly blogCategoryService: BlogCategoryService) {}
+
+    @Get('')
+    async cFilterSliders(@Res() res: Response) {
+        const categories = await this.blogCategoryService.getAllBlogCategories();
+        return res.send(categories);
+    }
+}
