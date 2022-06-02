ef54dcbd59651db7afd0e3d7e3422e3247e0dc00
Loc <hoanglocst900@gmail.com>
2022-06-02 11:23:10 +0700

    FIX - blog & subject filter guard

--- src/blog/blogs.controller.ts
+++ src/blog/blogs.controller.ts
@@ -1,14 +1,12 @@
 import { FilterBlogsDTO, vFilterBlogsDTO } from './dto';
 import { QueryJoiValidatorPipe } from './../core/pipe';
-import { MarketingGuard } from './../auth/guard';
 import { BlogService } from './blog.service';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
-import { Controller, UsePipes, Res, UseGuards, Get, Query } from '@nestjs/common';
+import { Controller, UsePipes, Res, Get, Query } from '@nestjs/common';
 import { Response } from 'express';
 
 @ApiTags('blogs')
 @ApiBearerAuth()
-@UseGuards(MarketingGuard)
 @Controller('blogs')
 export class BlogsController {
     constructor(private readonly blogService: BlogService) {}
