c0c85e13e6e27c361b37028136a38105763810df
loc <hoanglocst900@gmail.com>
2022-06-03 13:15:38 +0700

    ADD - get all lesson types

--- src/lesson-type/lesson-type.service.ts
+++ src/lesson-type/lesson-type.service.ts
@@ -1,7 +1,12 @@
+import { LessonType } from './../core/models/lesson-type';
 import { LessonTypeRepository } from './../core/repositories';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class LessonTypeService {
     constructor(private readonly lessonTypeRepository: LessonTypeRepository) {}
+
+    async getAllBlogCategories(): Promise<LessonType[]> {
+        return await this.lessonTypeRepository.createQueryBuilder('LessonType').getMany();
+    }
 }
