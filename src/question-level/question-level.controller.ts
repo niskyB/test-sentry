50c8ffd2e673843a9723adb5dda64045e4e05fa5
Loc <hoanglocst900@gmail.com>
2022-06-21 16:52:39 +0700

    ADD - get question level by id

--- src/question-level/question-level.controller.ts
+++ src/question-level/question-level.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { StatusCodes } from 'http-status-codes';
+import { ResponseMessage } from '../core/interface';
+import { Controller, Res, Get, Param, HttpException } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiParam } from '@nestjs/swagger';
+import { Response } from 'express';
+import { QuestionLevelService } from './question-level.service';
 
+@ApiTags('question level')
+@ApiBearerAuth()
 @Controller('question-level')
-export class QuestionLevelController {}
+export class QuestionLevelController {
+    constructor(private readonly questionLevelService: QuestionLevelService) {}
+
+    @Get('/:id')
+    @ApiParam({ name: 'id', example: 'TVgJIjsRFmIvyjUeBOLv4gOD3eQZY' })
+    async cGetQuestionLevelById(@Param('id') id: string, @Res() res: Response) {
+        const questionLevel = await this.questionLevelService.getOneByField('id', id);
+
+        if (!questionLevel) throw new HttpException({ errorMessage: ResponseMessage.NOT_FOUND }, StatusCodes.NOT_FOUND);
+        return res.send(questionLevel);
+    }
+}
