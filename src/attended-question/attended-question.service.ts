40b7c20e2237ae15b9d825a3ad812e2b4680dfb9
loc <hoanglocst900@gmail.com>
2022-06-22 22:32:52 +0700

    ADD - edit quiz

--- src/attended-question/attended-question.service.ts
+++ src/attended-question/attended-question.service.ts
@@ -1,4 +1,17 @@
+import { AttendedQuestionRepository } from './../core/repositories';
 import { Injectable } from '@nestjs/common';
+import { AttendedQuestion } from '../core/models';
 
 @Injectable()
-export class AttendedQuestionService {}
+export class AttendedQuestionService {
+    constructor(private readonly attendedQuestionRepository: AttendedQuestionRepository) {}
+
+    async getAttendedQuestionByQuizId(id: string): Promise<AttendedQuestion> {
+        return await this.attendedQuestionRepository
+            .createQueryBuilder('attended_question')
+            .leftJoinAndSelect('attended_question.questionInQuiz', 'questionInQuiz')
+            .leftJoinAndSelect('questionInQuiz.quiz', 'quiz')
+            .where('quiz.id = (:id)', { id })
+            .getOne();
+    }
+}
