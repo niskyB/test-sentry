a28178ef42d83465cd34c1548db324001f19f54d
loc <hoanglocst900@gmail.com>
2022-06-25 02:09:16 +0700

    ADD - update specific information of registration

--- src/registration/dto/index.ts
+++ src/registration/dto/index.ts
@@ -2,3 +2,4 @@ export * from './createRegistration.dto';
 export * from './updateGeneralInformation.dto';
 export * from './filterRegistrations.dto';
 export * from './filterMyRegistrations.dto';
+export * from './updateSpecificInformation.dto';
