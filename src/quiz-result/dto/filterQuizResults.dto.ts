4ab39e7a133a0bb8590d3b59093e0eaed06997d9
loc <hoanglocst900@gmail.com>
2022-06-26 22:15:56 +0700

    FIX - get quiz results

--- src/quiz-result/dto/filterQuizResults.dto.ts
+++ src/quiz-result/dto/filterQuizResults.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import * as joi from 'joi';
 export class FilterQuizResultsDTO {
+    @ApiProperty({ description: 'Subject', example: '123-sadf-adsf', nullable: true })
+    subject: string;
+
     @ApiProperty({ description: 'Current Page', example: '0', nullable: true })
     currentPage: number;
 
@@ -9,6 +12,7 @@ export class FilterQuizResultsDTO {
 }
 
 export const vFilterQuizResultsDTO = joi.object<FilterQuizResultsDTO>({
+    subject: joi.string().required().failover(''),
     currentPage: joi.number().min(0).required().failover(0),
     pageSize: joi.number().min(1).required().failover(4),
 });
