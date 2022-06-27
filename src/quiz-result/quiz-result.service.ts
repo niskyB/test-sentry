7dc3320ef9fe7615ee2ae85a3f947829b5db2a3e
loc <hoanglocst900@gmail.com>
2022-06-27 15:28:32 +0700

    FIX - query quiz result

--- src/quiz-result/quiz-result.service.ts
+++ src/quiz-result/quiz-result.service.ts
@@ -19,6 +19,7 @@ export class QuizResultService {
             .createQueryBuilder('quiz_result')
             .where(`quiz_result.${field.toString()} = (:value)`, { value })
             .leftJoinAndSelect('quiz_result.attendedQuestions', 'attendedQuestions')
+            .leftJoinAndSelect('attendedQuestions.userAnswers', 'userAnswers')
             .leftJoinAndSelect('attendedQuestions.questionInQuiz', 'questionInQuiz')
             .leftJoinAndSelect('questionInQuiz.question', 'question')
             .leftJoinAndSelect('questionInQuiz.quiz', 'quiz')
