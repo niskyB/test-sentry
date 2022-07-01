a81f5b756b92ddb6bc573f367268de6fc6b06173
loc <hoanglocst900@gmail.com>
2022-07-01 21:43:57 +0700

    FIX - filter blog categories

--- src/blog-category/blog-category.service.ts
+++ src/blog-category/blog-category.service.ts
@@ -27,16 +27,16 @@ export class BlogCategoryService {
 
     async filterBlogCategories(status: boolean, value: string, order: SortOrder, orderBy: string, currentPage: number, pageSize: number): Promise<{ data: BlogCategory[]; count: number }> {
         let blogCategories, count;
-        const statusValue = this.filterService.getMinMaxValue(status);
+        const isActiveValue = this.filterService.getMinMaxValue(status);
         try {
             blogCategories = await this.blogCategoryRepository
                 .createQueryBuilder('BlogCategory')
                 .where('BlogCategory.value LIKE (:value)', { value: `%${value}%` })
                 .andWhere(
                     new Brackets((qb) => {
-                        qb.where('BlogCategory.status = :statusMinValue', {
-                            statusMinValue: statusValue.minValue,
-                        }).orWhere('BlogCategory.status = :statusMaxValue', { statusMaxValue: statusValue.maxValue });
+                        qb.where('BlogCategory.isActive = :isActiveMinValue', {
+                            isActiveMinValue: isActiveValue.minValue,
+                        }).orWhere('BlogCategory.isActive = :isActiveMaxValue', { isActiveMaxValue: isActiveValue.maxValue });
                     }),
                 )
                 .orderBy(`BlogCategory.${orderBy}`, order)
@@ -50,8 +50,8 @@ export class BlogCategoryService {
                 .andWhere(
                     new Brackets((qb) => {
                         qb.where('BlogCategory.status = :statusMinValue', {
-                            statusMinValue: statusValue.minValue,
-                        }).orWhere('BlogCategory.status = :statusMaxValue', { statusMaxValue: statusValue.maxValue });
+                            statusMinValue: isActiveValue.minValue,
+                        }).orWhere('BlogCategory.status = :statusMaxValue', { statusMaxValue: isActiveValue.maxValue });
                     }),
                 )
                 .getCount();
